cc5c8f26e53eedb98d18ef073048086a0bc5c051
Igor Moleiro <igor.moleiro@gmail.com>
2021-06-22 19:59:20 -0300

    Initial commit

---
+++ pratical-java/src/main/java/com/course/praticaljava/PraticalJavaApplication.java
@@ -0,0 +1,13 @@
+package com.course.praticaljava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PraticalJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PraticalJavaApplication.class, args);
+	}
+
+}
